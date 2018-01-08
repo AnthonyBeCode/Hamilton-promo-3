d484b87fc3ffdbefb6f1d20c1bc7cd54ffa86bc8
Teddy Kishi <teddy.tdk@gmail.com>
2018-01-08 12:05:12 +0100

    Logique

--- Parcours/05-JavaScript/01-VARIABLES/main.js
+++ Parcours/05-JavaScript/01-VARIABLES/main.js
@@ -1,5 +1,39 @@
 
 
+let number_1 = 10; // number
+let number_2 = "10"; // string
+
+if ( number_1 > number_2 ) {
+  console.log("It is higher !!!");
+} else if ( number_1 < number_2 ) {
+  console.log("it is less");
+} else {
+  console.log("it is equal");
+}
+
+let myAge = 20;
+switch(myAge){
+  case 20 :
+    console.log("same");
+  break;
+  case (myAge > 20) :
+    console.log("old");
+  break;
+  case (myAge < 20) :
+    console.log("young");
+  break;
+  default :
+    console.log("number expected");
+}
+
+
+
+
+
+
+
+
+
 
 
 
