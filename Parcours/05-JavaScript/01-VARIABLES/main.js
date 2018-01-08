c9ecde478eebde08b03e4b85830ac269650ae007
Teddy Kishi <teddy.tdk@gmail.com>
2018-01-08 16:08:32 +0100

    Functions

--- Parcours/05-JavaScript/01-VARIABLES/main.js
+++ Parcours/05-JavaScript/01-VARIABLES/main.js
@@ -1,40 +1,15 @@
 
-
-let number_1 = 10; // number
-let number_2 = "10"; // string
-
-if ( number_1 > number_2 ) {
-  console.log("It is higher !!!");
-} else if ( number_1 < number_2 ) {
-  console.log("it is less");
-} else {
-  console.log("it is equal");
+function hello(word = "world", secondParam = "someone else") {
+  return "hello " + word + " and " + secondParam;
 }
 
-let myAge = 20;
-switch(myAge){
-  case 20 :
-    console.log("same");
-  break;
-  case (myAge > 20) :
-    console.log("old");
-  break;
-  case (myAge < 20) :
-    console.log("young");
-  break;
-  default :
-    console.log("number expected");
-}
+let yolo = hello("teddy"); // hello teddy and someone else
+let yolo1 = hello("imad"); // hello teddy and someone else
+let yolo2 = hello("laurent", "flavi"); // hello teddy and someone else
 
+let result = yolo + yolo1 + yolo2;
 
-
-
-
-
-
-
-
-
+console.log( result );
 
 
 
