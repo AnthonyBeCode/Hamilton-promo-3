146350b72c1c3c0b2b8c8b91727c88ef105204c4
Teddy Kishi <teddy.tdk@gmail.com>
2018-01-10 12:38:09 +0100

    Exemples

--- Parcours/05-JavaScript/01-VARIABLES/main.js
+++ Parcours/05-JavaScript/01-VARIABLES/main.js
@@ -1,5 +1,5 @@
 
-
+/*
 let resultMax = Math.max(-1, 3, 5, 9);
 let resultMin = Math.min(-1, 3, 5, 9);
 
@@ -22,7 +22,7 @@ for (let i = 0; i < 10; i++) {
   console.log( fruits[roundedNumber], rndNumber, roundedNumber )
 }
 
-
+*/
 
 /*
 
@@ -33,3 +33,85 @@ console.log( roundDown )
 
 
 */
+
+
+
+let number = 20;
+let strings = "Hello";
+let arrays = [];
+let functions = function(){};
+let booleans = true;
+let more = "...";
+
+let lotOfStringValues = ["string","string","string"];
+let lotOfNumberValues = [3,2,1];
+
+let lotOfMixedValues = [3,"string",1,"numbers"];
+
+let lotOfMixedValuesAndOthers = [
+  13,
+  "string",
+  [74,564,"somthing"],
+  function(){
+    console.log("yolo");
+  },
+  true,
+  false
+];
+/*
+console.log(lotOfStringValues)
+console.log(lotOfNumberValues)
+console.log(lotOfMixedValues)
+console.log(lotOfMixedValuesAndOthers)
+
+console.log( lotOfMixedValuesAndOthers[1] );
+
+console.log( lotOfMixedValuesAndOthers[3]() );
+
+let something = lotOfMixedValuesAndOthers[2][2];
+*/
+
+
+// let arrays = [];
+// let myFn function (){}
+// myFn();
+
+//let object = new Object();
+//
+let object = {}; // JSON object
+
+let lotOfMixedValuesObject = {
+  "number" : 13,
+  myString : "string",
+  "arrays" : [74, 564, "somthing"],
+  "sayYolo" : function(){
+    console.log("yolo");
+  },
+  "myBollean" : true,
+  "amISad" : false
+};
+
+
+
+console.log( lotOfMixedValuesObject )
+console.log( lotOfMixedValuesObject.myString )
+console.log( lotOfMixedValuesObject["myString"] )
+
+
+for ( let properties in lotOfMixedValuesObject ) {
+  console.log( properties ) // name of the property
+  console.log( lotOfMixedValuesObject[properties] ) // get value of the property
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
